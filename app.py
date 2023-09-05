5bc1921947736277f3cf7e82fee2cd36522795a1
Leone <leone.tg@outlook.com>
2023-09-05 01:11:27 +0000

    layout e entradas

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask('meu app')
+
+
+posts = [
+    {
+        "titulo": "minha primeira postagem",
+        "texto": "teste"
+    },
+    {
+        "titulo": "segunda",
+        "texto": "teste 2"
+    }
+]
 
 @app.route('/')
 
-def hello():
-    return 'hello world'
+def exibir_entradas():
+    entradas = posts
+    return render_template(exibir_entradas)
+
+
+
+    @app.route('/')
+def exibir_entradas():
+    sql = '''select titulo, texto from entriadas order by id desc'''
+    cur = g.db.execute(sql)
+    entradas = [dict(titulo=titulo, texto=texto)
+                    for titulo, texto in cur.fetchall()]
+    return render_template('exibir_entradas.html', entradas=entradas)
